4dd832d2cea70632520323b7b1106893a5c70d40
dedehermawan <alidefit@gmail.com>
2017-03-13 13:16:43 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 		'home#index'
-  get 		'home/index'
-  get 		'home/help'
-  get 		'home/about'
+  get 'users/new'
+
+  root 'home#index'
+  get  '/help',    to: 'home#help'
+  get  '/about',   to: 'home#about'
+  get  '/contact', to: 'home#contact'
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
